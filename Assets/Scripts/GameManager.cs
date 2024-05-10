58c7b2d1c6db1d7e41882c30072b77052e0c6146
hernanbrue <hjbrue@gmail.com>
2024-05-10 09:14:27 -0300

    Logic improved and updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,6 +9,14 @@ public class GameManager : MonoBehaviour
     public List<question> responseList; //lista donde guardo la respuesta de la query hecha en la pantalla de selección de categoría
 
     public int currentTriviaIndex = 0;
+
+    public int randomQuestionIndex = 0;
+
+    public List<string> _answers = new List<string>();
+
+    public bool queryCalled;
+
+    string _correctAnswer;
 
     public static GameManager Instance { get; private set; }
 
@@ -33,6 +42,8 @@ public class GameManager : MonoBehaviour
 
         StartTrivia();
 
+        queryCalled = false;
+
     }
 
     void StartTrivia()
@@ -44,19 +55,44 @@ public class GameManager : MonoBehaviour
 
     }
 
-    //public void NextQuestion()
-    //{
-    //    currentTriviaIndex++;
-    //    if (currentTriviaIndex < triviaManager.totalTrivias)
-    //    {
-    //        StartTrivia();
-    //    }
-    //    else
-    //    {
-    //        // Mostrar puntaje final y guardar en la base de datos
-    //        // Mostrar ranking
-    //    }
-    //}
+    public void CategoryAndQuestionQuery(bool isCalled)
+    {
+        isCalled = UIManagment.Instance.queryCalled;
+
+        if (!isCalled)
+        {
+
+            randomQuestionIndex = Random.Range(0, GameManager.Instance.responseList.Count);
+
+            //_questionText.text = GameManager.Instance.responseList[randomQuestionIndex].QuestionText;
+            _correctAnswer = GameManager.Instance.responseList[randomQuestionIndex].CorrectOption;
+
+            //agrego a la lista de answers las 3 answers
+
+            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer1);
+            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer2);
+            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer3);
+
+            // la mixeo con el método Shuffle (ver script Shuffle List)
+
+            _answers.Shuffle();
+
+            // asigno estos elementos a los textos de los botones
+
+            for (int i = 0; i < UIManagment.Instance._buttons.Length; i++)
+            {
+                UIManagment.Instance._buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = _answers[i];
+
+                int index = i; // Captura el valor actual de i en una variable local -- SINO NO FUNCA!
+
+                UIManagment.Instance._buttons[i].onClick.AddListener(() => UIManagment.Instance.OnButtonClick(index));
+            }
+
+
+            UIManagment.Instance.queryCalled = true;
+        }
+
+    }
 
     private void Update()
     {
--- Assets/UIManagment.cs
+++ Assets/UIManagment.cs
@@ -13,13 +13,31 @@ public class UIManagment : MonoBehaviour
     
     string _correctAnswer;
 
-    [SerializeField] Button[] _buttons = new Button[3];
+    public Button[] _buttons = new Button[3];
 
     private List<string> _answers = new List<string>();
 
-    private bool queryCalled;
+    public bool queryCalled;
 
     private Color _originalButtonColor;
+
+    public static UIManagment Instance { get; private set; }
+
+
+    void Awake()
+    {
+        // Configura la instancia
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Para mantener el objeto entre escenas
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
 
 
     private void Start()
@@ -27,54 +45,20 @@ public class UIManagment : MonoBehaviour
         queryCalled = false;
 
         _originalButtonColor = _buttons[0].GetComponent<Image>().color;
+
     }
 
     void Update()
     {
-        CategoryAndQuestionQuery();
+        _categoryText.text = PlayerPrefs.GetString("SelectedTrivia");
+        _questionText.text = GameManager.Instance.responseList[GameManager.Instance.randomQuestionIndex].QuestionText;
+
+        GameManager.Instance.CategoryAndQuestionQuery(queryCalled);
 
     }
-
-    private void CategoryAndQuestionQuery()
+    public void OnButtonClick(int buttonIndex)
     {
-        if (!queryCalled)
-        {
-            _categoryText.text = PlayerPrefs.GetString("SelectedTrivia");
-
-            int randomQuestionIndex = Random.Range(0, GameManager.Instance.responseList.Count);
-
-            _questionText.text = GameManager.Instance.responseList[randomQuestionIndex].QuestionText;
-            _correctAnswer = GameManager.Instance.responseList[randomQuestionIndex].CorrectOption;
-
-            //agrego a la lista de answers las 3 answers
-
-            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer1);
-            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer2);
-            _answers.Add(GameManager.Instance.responseList[randomQuestionIndex].Answer3);
-
-            // la mixeo con el método Shuffle (ver script Shuffle List)
-
-            _answers.Shuffle();
-
-            // asigno estos elementos a los textos de los botones
-
-            for (int i = 0; i < _buttons.Length; i++)
-            {
-                _buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = _answers[i];
-
-                int index = i; // Captura el valor actual de i en una variable local -- SINO NO FUNCA!
-
-                _buttons[i].onClick.AddListener(() => OnButtonClick(index));
-            }
-
-
-            queryCalled = true;
-        }
-                
-    }
-
-    private void OnButtonClick(int buttonIndex)
-    {
+        _correctAnswer = GameManager.Instance.responseList[GameManager.Instance.randomQuestionIndex].CorrectOption;
         string selectedAnswer = _buttons[buttonIndex].GetComponentInChildren<TextMeshProUGUI>().text;
 
         if (selectedAnswer == _correctAnswer)
@@ -82,7 +66,9 @@ public class UIManagment : MonoBehaviour
             Debug.Log("¡Respuesta correcta!");
             ChangeButtonColor(buttonIndex, Color.green);
             Invoke("RestoreButtonColor", 2f);
+            GameManager.Instance._answers.Clear();
             Invoke("NextAnswer", 2f);
+            
         }
         else
         {
